661786881a29f5e1f1dcb8ba77615cf837b9e6ad
Harshit <chauhan439@gmail.com>
2020-04-30 13:23:13 +0530

    added comments

--- pangram.jsx
+++ pangram.jsx
@@ -1,7 +1,10 @@
 function pangram(input){
+    
+    //transform string into array
     let strArr= input.toLowerCase().split("") ;
+    //remove white space and punctuations and duplicates
     strArr= strArr.filter((item, index) => strArr.indexOf(item)===index && (item.charCodeAt(0)>=97 && item.charCodeAt(0)<=122)) ;
-
+    //number of alphabets
     if(strArr.length===26)
         return true ;
     else 
