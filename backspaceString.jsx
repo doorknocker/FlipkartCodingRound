a9d0c710c9409bb7b7d581f4722f0e019b2fe0c9
Harshit <chauhan439@gmail.com>
2020-04-30 13:16:17 +0530

    added comments to backspace string solution

--- backspaceString.jsx
+++ backspaceString.jsx
@@ -1,13 +1,19 @@
 function backspaceString (input){
+    //edge case
     if(input.length===0)
         return "" ;
+
+    //transform string into array
     let strArr= input.split("") ;
     if(input.includes("#")){
+        //find the hash
         let pos= strArr.indexOf("#") ;
         strArr= strArr.filter((item, index) => (index!== pos && index!== pos-1)) ;
+        //recursive function for the new string
         return backspaceString(strArr.join("")) ;        
     }
     else{
+        //if no backspace string is found
         return input ;
     }
    
